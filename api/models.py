df1bc0f85e5d3ad74d0351b110d0e2e0787d1891
me <my@email.com>
2018-05-26 23:11:13 +0300

    Add registration

--- api/models.py
+++ api/models.py
@@ -1,16 +1,34 @@
-from api import db, ma
+from api import db, ma, login_manager
+from flask_login import UserMixin
 
 class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     key = db.Column(db.String(80))
     value = db.Column(db.String(80))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def __init__(self, key, value):
+    def __init__(self, key, value, user_id):
         self.key = key
         self.value = value
+        self.user_id = user_id
 
 class EntrySchema(ma.Schema):
     class Meta:
-        fields = ('key', 'value')
+        fields = ('key', 'value', 'user_id')
 
 entry_schema = EntrySchema()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    entries = db.relationship('Entry', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
--- api/views.py
+++ api/views.py
@@ -1,10 +1,13 @@
-from flask import Flask, request, jsonify, render_template
+from flask import jsonify, render_template, url_for, flash, redirect, request
 from wtforms.validators import ValidationError
 
-from api import app, db
-from api.models import entry_schema, Entry
+from api import app, db, bcrypt
+from api.forms import RegistrationForm, LoginForm
+from api.models import entry_schema, Entry, User
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route("/set/", methods=["GET", "POST"])
+@login_required
 def set_entry():
     if request.method == 'POST':
         json_data = request.get_json()
@@ -16,10 +19,10 @@ def set_entry():
                     data = entry_schema.load(json_data)
             except ValidationError as err:
                 return jsonify(err.messages), 422
-            key, value = data['key'], data['value']
+            key, value, user_id = data['key'], data['value'], current_user.id
             entry = Entry.query.filter_by(key=key).first()
             if entry is None:
-                entry = Entry(key=key, value=value)
+                entry = Entry(key=key, value=value, user_id=user_id)
                 db.session.add(entry)
                 db.session.commit()
                 return jsonify({'value': 'The entry with key {} was succesfully created'.format(key)})
@@ -31,6 +34,7 @@ def set_entry():
 
 @app.route("/get/", methods=["GET"])
 @app.route("/get/<key>", methods=["GET"])
+@login_required
 def get_entry(key=None):
     if key is None and not request.is_xhr:
         return render_template('get.html')
@@ -40,3 +44,46 @@ def get_entry(key=None):
         return entry_schema.jsonify(entry)
     else:
         return jsonify({'value': 'No value with this key in db'})
+
+### register views
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('set_entry'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('set_entry'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('set_entry'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@login_required
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
